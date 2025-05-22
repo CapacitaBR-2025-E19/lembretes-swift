1ade3d054bc225baff45ec7c4f63a11b4036fe1e
CapacitaBR-2025-E19 <equipe19.ioscapacitabr@gmail.com>
2025-05-22 18:05:28 -0300

    Commit Inicial || -Enzo M.S.

---
+++ lembretes/lembretes/Model/LembreteModel.swift
@@ -0,0 +1,17 @@
+//
+//  LembreteModel.swift
+//  lembretes
+//
+//  Created by iredefbmac_29 on 22/05/25.
+//
+
+import Foundation
+
+class LembreteModel {
+    var date:Int
+    
+    
+    init(date dateInput: Int) {
+        date = dateInput
+    }
+}
---
+++ lembretes/lembretes/lembretesApp.swift
@@ -0,0 +1,17 @@
+//
+//  lembretesApp.swift
+//  lembretes
+//
+//  Created by iredefbmac_29 on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct lembretesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
