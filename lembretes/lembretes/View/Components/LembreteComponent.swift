524015538e4d4d7daaf3ca85230d418b077e5a80
CapacitaBR-2025-E19 <equipe19.ioscapacitabr@gmail.com>
2025-05-25 15:03:15 -0300

    Componente de Lembretes incompleto, completo pra POC || -Enzo M.S.

--- lembretes/lembretes/View/Components/LembreteComponent.swift
+++ lembretes/lembretes/View/Components/LembreteComponent.swift
@@ -19,25 +19,27 @@ struct LembreteComponent: View {
         // Me perdoar por qualquer erro brutal, são 2:30 da manhã -Enzo
         
         ZStack(alignment: .top) {
+            // Retângulo Primario
             RoundedRectangle(cornerRadius: 25)
-                .padding(20)
+                .foregroundStyle(Color.white2)
+                .padding(10)
                 .frame(minHeight: 200, maxHeight: 200)
-                .foregroundStyle(Color.gray)
             
-            VStack() {
-                // Elementos do Topo
+            VStack(alignment: .leading, spacing: 0) {
+                // Top Start
                 ZStack(alignment: .trailing) {
-                    RoundedRectangle(cornerRadius: 25)
-                        .padding(25)
-                        .frame(height: 90)
+                    RoundedRectangle(cornerRadius: 20)
                         .foregroundStyle(Color.white)
+                        .padding([.horizontal, .top], 15)
+                        .frame(height: 90)
                     
                     HStack {
                         Spacer()
                             .frame(width: 5)
                         
                         TextField("Título do Lembrete", text: name) // limitar de alguma forma o tamanho desse bixo
-                            .font(.system(size: 14))
+                            .font(.system(size: 14, weight: .bold))
+                            .foregroundStyle(Color.white2)
                         
                         Spacer()
                             .frame(width: 5)
@@ -45,12 +47,65 @@ struct LembreteComponent: View {
                         Toggle("Toggle do Lembrete", isOn:isEnabled)
                             .labelsHidden()
                             .toggleStyle(.switch)
-                    } .padding(30)
+                            .padding(.leading, 10)
+                    } .padding(.top, 15)
+                        .padding(.horizontal, 30)
                 }
+                // Top End
                 
                 // Elementos Interativos Inferiores (Data, Toque/Toggles de Notif e Vibração, Descrição)
                 // No futuro isso aqui seria bom ser colapsível
                 
+                HStack(spacing: 0) {
+                    // Time Button Start
+                    Button(
+                        action:{
+                        
+                        },
+                        
+                        label:{
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .foregroundStyle(Color.white)
+                                    .frame(width: 277.5, height: 90)
+                                    .padding(.leading, 15)
+                                    .padding(.top, 5)
+                                Text(date.wrappedValue.formatted())
+                                    .font(.system(size: 28, weight: .semibold))
+                                    .foregroundStyle(Color.white2)
+                                    .padding(.leading, 15)
+                                    .padding(.top, 5)
+                            }
+                        }
+                    )
+                    // Time Button End
+
+                    // Expand Button Start
+                    Button(
+                        action:{
+                            
+                        },
+                        label:{
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .foregroundStyle(Color.white)
+                                    .frame(width: 90, height: 90)
+                                    .padding(.leading, 5)
+                                    .padding(.trailing, 15)
+                                    .padding(.top, 5)
+                                    .overlay(
+                                        Image(systemName: "arrowshape.down.fill")
+                                            .font(.system(size: 45))
+                                            .padding(.leading, 5)
+                                            .padding(.trailing, 15)
+                                            .padding(.top, 5)
+                                            .foregroundStyle(Color.white2)
+                                    )
+                            }
+                        }
+                    )
+                    // Expand Button End
+                }
             }
         }
     }
